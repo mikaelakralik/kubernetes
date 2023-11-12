8716b2c642fb297f1865b27f11c0133dda91060e
mikaelakralik <139630743+mikaelakralik@users.noreply.github.com>
2023-11-12 19:17:39 +0200

    Update apparmor_test.go

--- test/e2e_node/apparmor_test.go
+++ test/e2e_node/apparmor_test.go
@@ -28,7 +28,7 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v2"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
